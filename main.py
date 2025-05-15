5eb425cf916c728ffc200d3e561e254d0cd2ac99
Seifii <sofiapetrova893@gmail.com>
2025-05-15 17:51:59 +0300

    end

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-try:
-    print("start code")
-    print(10/0)
-    print("no errors")
-except (NameError, ZeroDivisionError):
-    print("we have an Error")
+# words = ["apple", "banana", "cherry", "date"]
+# try:
+#     index = int(input("Введіть індекс: "))
+#     word = words[index]
+#     print("Слово за індексом", index, ":", word)
+# except IndexError:
+#     print("Занадто довгий список")
+# except ValueError:
+#     print("Не є числом")
+#
+# raise
 
-print("code after capsule")
+correct_password = "1234"
+attempt = 3
+while attempt > 0:
+    try:
+        password = input("Input password: ")
+        if password == correct_password:
+            print("Yaaaay")
+            break
+        else:
+            attempt -= 1
+        print(f"X ne correct pass")
+
+        if attempt == 0:
+            raise PermissionError("Usyo")
+
+
+    except PermissionError as error:
+        print(error)
